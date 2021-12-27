5b899bfad8f28514d96803b419e4cdd39f78695d
Reinhold Gschweicher <pyro4hell@gmail.com>
2021-12-27 15:39:38 +0100

    PassKey: port to LVGL8

--- src/displayapp/screens/PassKey.cpp
+++ src/displayapp/screens/PassKey.cpp
@@ -4,15 +4,15 @@
 using namespace Pinetime::Applications::Screens;
 
 PassKey::PassKey(Pinetime::Applications::DisplayApp* app, uint32_t key) : Screen(app) {
-  passkeyLabel = lv_label_create(lv_scr_act(), nullptr);
-  lv_obj_set_style_local_text_color(passkeyLabel, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, lv_color_hex(0xFFFF00));
-  lv_obj_set_style_local_text_font(passkeyLabel, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, &jetbrains_mono_42);
+  passkeyLabel = lv_label_create(lv_scr_act());
+  lv_obj_set_style_text_color(passkeyLabel, lv_color_hex(0xFFFF00), LV_PART_MAIN | LV_STATE_DEFAULT);
+  lv_obj_set_style_text_font(passkeyLabel, &jetbrains_mono_42, LV_PART_MAIN | LV_STATE_DEFAULT);
   lv_label_set_text_fmt(passkeyLabel, "%06u", key);
-  lv_obj_align(passkeyLabel, nullptr, LV_ALIGN_CENTER, 0, -20);
+  lv_obj_align(passkeyLabel, LV_ALIGN_CENTER, 0, -20);
 
-  backgroundLabel = lv_label_create(lv_scr_act(), nullptr);
-  lv_obj_set_click(backgroundLabel, true);
-  lv_label_set_long_mode(backgroundLabel, LV_LABEL_LONG_CROP);
+  backgroundLabel = lv_label_create(lv_scr_act());
+  lv_obj_add_flag(backgroundLabel, LV_OBJ_FLAG_CLICKABLE);
+  lv_label_set_long_mode(backgroundLabel, LV_LABEL_LONG_CLIP);
   lv_obj_set_size(backgroundLabel, 240, 240);
   lv_obj_set_pos(backgroundLabel, 0, 0);
   lv_label_set_text(backgroundLabel, "");
