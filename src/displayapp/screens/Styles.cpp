8026e3db2b331926534de5206860b2b43d37fd76
Reinhold Gschweicher <pyro4hell@gmail.com>
2021-12-27 15:45:09 +0100

    Styles: port to LVGL8

--- src/displayapp/screens/Styles.cpp
+++ src/displayapp/screens/Styles.cpp
@@ -1,8 +1,8 @@
 #include "Styles.h"
 
 void Pinetime::Applications::Screens::SetRadioButtonStyle(lv_obj_t* checkbox) {
-  lv_obj_set_style_local_radius(checkbox, LV_CHECKBOX_PART_BULLET, LV_STATE_DEFAULT, LV_RADIUS_CIRCLE);
-  lv_obj_set_style_local_border_width(checkbox, LV_CHECKBOX_PART_BULLET, LV_STATE_CHECKED, 9);
-  lv_obj_set_style_local_border_color(checkbox, LV_CHECKBOX_PART_BULLET, LV_STATE_CHECKED, LV_COLOR_GREEN);
-  lv_obj_set_style_local_bg_color(checkbox, LV_CHECKBOX_PART_BULLET, LV_STATE_CHECKED, LV_COLOR_WHITE);
+  lv_obj_set_style_radius(checkbox, LV_RADIUS_CIRCLE, LV_PART_INDICATOR | LV_STATE_DEFAULT);
+  lv_obj_set_style_border_width(checkbox, 9, LV_PART_INDICATOR | LV_STATE_CHECKED);
+  lv_obj_set_style_border_color(checkbox, lv_palette_main(LV_PALETTE_GREEN), LV_PART_INDICATOR | LV_STATE_CHECKED);
+  lv_obj_set_style_bg_color(checkbox, lv_color_white(), LV_PART_INDICATOR | LV_STATE_CHECKED);
 }
